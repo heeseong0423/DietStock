11b60342e9ae452e7b2fe361ba875b576f9e673b
heeseong0423 <heeseong0423@naver.com>
2021-05-10 17:12:46 +0900

    로그인상태

--- app/src/main/java/com/fournineseven/dietstock/SplashActivity.kt
+++ app/src/main/java/com/fournineseven/dietstock/SplashActivity.kt
@@ -41,14 +41,14 @@ class SplashActivity : AppCompatActivity() {
 
     //필요한 권한들 정의
     private val fitnessOptions = FitnessOptions.builder()
-        .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
-        .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_WRITE)
-        .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-        .addDataType(DataType.TYPE_NUTRITION)
-        .addDataType(DataType.TYPE_CALORIES_EXPENDED)
-        .addDataType(DataType.AGGREGATE_NUTRITION_SUMMARY)
-        .addDataType(DataType.AGGREGATE_CALORIES_EXPENDED)
-        .build()
+            .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_WRITE)
+            .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+            .addDataType(DataType.TYPE_NUTRITION)
+            .addDataType(DataType.TYPE_CALORIES_EXPENDED)
+            .addDataType(DataType.AGGREGATE_NUTRITION_SUMMARY)
+            .addDataType(DataType.AGGREGATE_CALORIES_EXPENDED)
+            .build()
 
     //구글핏 접근을 위한 API Account 인스턴스 생성
     private val account by lazy {
@@ -64,12 +64,11 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
         /*val db = Room.databaseBuilder(
                 this,
                 KcalDatabase::class.java, "database-name"
         ).allowMainThreadQueries()
-            .build()
+                .build()
         val userDao = db.kcalDao()
 
         var userKcalData = UserKcalData(0, User.kcal,User.PKcal,User.startKcal,User.endKcal,User.highKcal,User.lowKcal)
@@ -77,7 +76,7 @@ class SplashActivity : AppCompatActivity() {
 */
         //오늘 날짜 얻기
         TimeCheck.appStartTime = LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT).atZone(ZoneId.systemDefault())
-            .toEpochSecond()
+                .toEpochSecond()
 
         checkPermission()
         Log.d(TAG, "포어그라운드 현재 상태 : $isActivityForeground")
@@ -112,12 +111,12 @@ class SplashActivity : AppCompatActivity() {
                 builder.setPositiveButton(R.string.ok, null)
                 builder.setOnDismissListener(DialogInterface.OnDismissListener {
                     requestPermissions(
-                        arrayOf(
-                            Manifest.permission.ACTIVITY_RECOGNITION,
-                            Manifest.permission.CAMERA,
-                            Manifest.permission.READ_EXTERNAL_STORAGE,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        ), PERMISSION_REQUEST_ACTIVITY
+                            arrayOf(
+                                    Manifest.permission.ACTIVITY_RECOGNITION,
+                                    Manifest.permission.CAMERA,
+                                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            ), PERMISSION_REQUEST_ACTIVITY
                     )
                 })
                 builder.show()
@@ -135,10 +134,10 @@ class SplashActivity : AppCompatActivity() {
     private fun googleSignInCheckPermission() {
         if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) { //권한 없을 경우 권한요청
             GoogleSignIn.requestPermissions( //호출 후 결과 요청을 onActivityResult에서 알려줌.
-                this, // your activity
-                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE, // e.g. 1
-                account,
-                fitnessOptions
+                    this, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE, // e.g. 1
+                    account,
+                    fitnessOptions
             )
 
         } else {//이미 권한들 체크했을 경우 코드 실행
@@ -176,16 +175,16 @@ class SplashActivity : AppCompatActivity() {
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             PERMISSION_REQUEST_ACTIVITY -> {
                 if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d(TAG, "구글 활동 허락했음.")
-                    googleSignInCheckPermission()
+                    //googleSignInCheckPermission()
                 } else {
                     Log.d(TAG, "거절했네")
                     finish()
@@ -200,55 +199,55 @@ class SplashActivity : AppCompatActivity() {
 
         //누적걷기 구독
         Fitness.getRecordingClient(
-            this,
-            GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+                this,
+                GoogleSignIn.getAccountForExtension(this, fitnessOptions)
         )
-            .subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE)
-            .addOnSuccessListener {
-                Log.i(TAG, "누적걷기수 구독 성공")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "누적걷기수 구독 오류 발생", e)
-            }
+                .subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                .addOnSuccessListener {
+                    Log.i(TAG, "누적걷기수 구독 성공")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "누적걷기수 구독 오류 발생", e)
+                }
 
         //걷기 구독
         Fitness.getRecordingClient(
-            this,
-            GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+                this,
+                GoogleSignIn.getAccountForExtension(this, fitnessOptions)
         )
-            .subscribe(DataType.TYPE_STEP_COUNT_DELTA)
-            .addOnSuccessListener {
-                Log.i(TAG, "걷기수 구독 성공")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "걷기수 구독 오류 발생 ", e)
-            }
+                .subscribe(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener {
+                    Log.i(TAG, "걷기수 구독 성공")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "걷기수 구독 오류 발생 ", e)
+                }
 
         //칼로리 총량 구독
         Fitness.getRecordingClient(
-            this,
-            GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+                this,
+                GoogleSignIn.getAccountForExtension(this, fitnessOptions)
         )
-            .subscribe(DataType.AGGREGATE_CALORIES_EXPENDED)
-            .addOnSuccessListener {
-                Log.i(TAG, "칼로리 총량 구독 성공")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "칼로리 총량 구독 오류 발생", e)
-            }
+                .subscribe(DataType.AGGREGATE_CALORIES_EXPENDED)
+                .addOnSuccessListener {
+                    Log.i(TAG, "칼로리 총량 구독 성공")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "칼로리 총량 구독 오류 발생", e)
+                }
 
         //칼로리 구독
         Fitness.getRecordingClient(
-            this,
-            GoogleSignIn.getAccountForExtension(this, fitnessOptions)
+                this,
+                GoogleSignIn.getAccountForExtension(this, fitnessOptions)
         )
-            .subscribe(DataType.TYPE_CALORIES_EXPENDED)
-            .addOnSuccessListener {
-                Log.i(TAG, "칼로리 구독 성공")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "칼로리 구독 오류 발생", e)
-            }
+                .subscribe(DataType.TYPE_CALORIES_EXPENDED)
+                .addOnSuccessListener {
+                    Log.i(TAG, "칼로리 구독 성공")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "칼로리 구독 오류 발생", e)
+                }
     }
 
     private fun readCalorie() {
@@ -257,22 +256,22 @@ class SplashActivity : AppCompatActivity() {
 
         //칼로리 총량 읽기
         val readRequest = DataReadRequest.Builder()
-            .aggregate(DataType.AGGREGATE_CALORIES_EXPENDED)
-            .bucketByActivityType(1, TimeUnit.SECONDS)
-            .setTimeRange(start, end.toEpochSecond(), TimeUnit.SECONDS)
-            .build()
+                .aggregate(DataType.AGGREGATE_CALORIES_EXPENDED)
+                .bucketByActivityType(1, TimeUnit.SECONDS)
+                .setTimeRange(start, end.toEpochSecond(), TimeUnit.SECONDS)
+                .build()
 
         Fitness.getHistoryClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
-            .readData(readRequest)
-            .addOnSuccessListener { response ->
-                // The aggregate query puts datasets into buckets, so flatten into a single list of datasets
+                .readData(readRequest)
+                .addOnSuccessListener { response ->
+                    // The aggregate query puts datasets into buckets, so flatten into a single list of datasets
 //                    for (dataSet in response.buckets.flatMap { it.dataSets }) {
 //                        dumpDataSet(dataSet)
 //                    }
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "There was an error reading data from Google Fit", e)
-            }
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "There was an error reading data from Google Fit", e)
+                }
     }
 
     fun dumpDataSet(dataSet: DataSet) {
@@ -291,49 +290,49 @@ class SplashActivity : AppCompatActivity() {
     }
 
     fun DataPoint.getStartTimeString() = Instant.ofEpochSecond(this.getStartTime(TimeUnit.SECONDS))
-        .atZone(ZoneId.systemDefault())
-        .toLocalDateTime().toString()
+            .atZone(ZoneId.systemDefault())
+            .toLocalDateTime().toString()
 
     fun DataPoint.getEndTimeString() = Instant.ofEpochSecond(this.getEndTime(TimeUnit.SECONDS))
-        .atZone(ZoneId.systemDefault())
-        .toLocalDateTime().toString()
+            .atZone(ZoneId.systemDefault())
+            .toLocalDateTime().toString()
 
     private fun readStepCount() {
         Fitness.getHistoryClient(this, account)
-            .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
-            .addOnSuccessListener { result ->
-                val totalSteps =
-                    result.dataPoints.firstOrNull()?.getValue(Field.FIELD_STEPS)?.asInt()
-                        ?: 0
-                // Use response data here
-                Log.i(TAG, "OnSuccess()")
-                Log.d(TAG, "걷기 수 : $totalSteps")
-                User.step = totalSteps
-            }
-            .addOnFailureListener { e ->
-                Log.d(TAG, "There was a problem getting steps.", e)
-            }
+                .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener { result ->
+                    val totalSteps =
+                            result.dataPoints.firstOrNull()?.getValue(Field.FIELD_STEPS)?.asInt()
+                                    ?: 0
+                    // Use response data here
+                    Log.i(TAG, "OnSuccess()")
+                    Log.d(TAG, "걷기 수 : $totalSteps")
+                    User.step = totalSteps
+                }
+                .addOnFailureListener { e ->
+                    Log.d(TAG, "There was a problem getting steps.", e)
+                }
 
 
         Fitness.getSensorsClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
-            .findDataSources(
-                DataSourcesRequest.Builder()
-                    .setDataTypes(DataType.TYPE_STEP_COUNT_DELTA, DataType.TYPE_CALORIES_EXPENDED)
-                    .setDataSourceTypes(DataSource.TYPE_RAW)
-                    .build())
-            .addOnSuccessListener { dataSources ->
-                dataSources.forEach {
-                    Log.i(TAG, "Data source found: ${it.streamIdentifier}")
-                    Log.i(TAG, "Data Source type: ${it.dataType.name}")
+                .findDataSources(
+                        DataSourcesRequest.Builder()
+                                .setDataTypes(DataType.TYPE_STEP_COUNT_DELTA, DataType.TYPE_CALORIES_EXPENDED)
+                                .setDataSourceTypes(DataSource.TYPE_RAW)
+                                .build())
+                .addOnSuccessListener { dataSources ->
+                    dataSources.forEach {
+                        Log.i(TAG, "Data source found: ${it.streamIdentifier}")
+                        Log.i(TAG, "Data Source type: ${it.dataType.name}")
 
-                    if (it.dataType == DataType.TYPE_STEP_COUNT_DELTA) {
-                        Log.i(TAG, "Data source for STEP_COUNT_DELTA found!")
+                        if (it.dataType == DataType.TYPE_STEP_COUNT_DELTA) {
+                            Log.i(TAG, "Data source for STEP_COUNT_DELTA found!")
+                        }
                     }
                 }
-            }
-            .addOnFailureListener { e ->
-                Log.e(TAG, "Find data sources request failed", e)
-            }
+                .addOnFailureListener { e ->
+                    Log.e(TAG, "Find data sources request failed", e)
+                }
 
         val listener = OnDataPointListener { dataPoint ->
             for (field in dataPoint.dataType.fields) {
@@ -350,37 +349,37 @@ class SplashActivity : AppCompatActivity() {
         }
 
         Fitness.getSensorsClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
-            .add(
-                SensorRequest.Builder()
-                    //.setDataSource() // Optional but recommended for custom
-                    // data sets.
-                    .setDataType(DataType.TYPE_STEP_COUNT_DELTA) // Can't be omitted.
-                    .setSamplingRate(10, TimeUnit.SECONDS)
-                    .build(),
-                listener
-            )
-            .addOnSuccessListener {
-                Log.i(TAG, "Listener registered!")
-            }
-            .addOnFailureListener {
-                Log.e(TAG, "Listener not registered.")
-            }
+                .add(
+                        SensorRequest.Builder()
+                                //.setDataSource() // Optional but recommended for custom
+                                // data sets.
+                                .setDataType(DataType.TYPE_STEP_COUNT_DELTA) // Can't be omitted.
+                                .setSamplingRate(10, TimeUnit.SECONDS)
+                                .build(),
+                        listener
+                )
+                .addOnSuccessListener {
+                    Log.i(TAG, "Listener registered!")
+                }
+                .addOnFailureListener {
+                    Log.e(TAG, "Listener not registered.")
+                }
 
         Fitness.getSensorsClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
-            .add(
-                SensorRequest.Builder()
-                    //.setDataSource() // Optional but recommended for custom
-                    // data sets.
-                    .setDataType(DataType.TYPE_CALORIES_EXPENDED) // Can't be omitted.
-                    .setSamplingRate(10, TimeUnit.SECONDS)
-                    .build(),
-                listener
-            )
-            .addOnSuccessListener {
-                Log.i(TAG, "칼로리 리스너 등록 성공")
-            }
-            .addOnFailureListener {
-                Log.e(TAG, "칼로리 리스너 등록 실패")
-            }
+                .add(
+                        SensorRequest.Builder()
+                                //.setDataSource() // Optional but recommended for custom
+                                // data sets.
+                                .setDataType(DataType.TYPE_CALORIES_EXPENDED) // Can't be omitted.
+                                .setSamplingRate(10, TimeUnit.SECONDS)
+                                .build(),
+                        listener
+                )
+                .addOnSuccessListener {
+                    Log.i(TAG, "칼로리 리스너 등록 성공")
+                }
+                .addOnFailureListener {
+                    Log.e(TAG, "칼로리 리스너 등록 실패")
+                }
     }
 }

cddafaace40ce3200a86cc54b1b7fe813440b2df
heeseong0423 <heeseong0423@naver.com>
2021-05-21 15:07:05 +0900

    my page

--- app/src/main/java/com/fournineseven/dietstock/LoginState.kt
+++ app/src/main/java/com/fournineseven/dietstock/LoginState.kt
@@ -22,10 +22,19 @@ object LoginState {
     const val BEFORE_IMAGE_KEY = "before_image"
     const val AFTER_IMAGE_KEY = "after_image"
 
+    const val GOAL_KEY = "goal_key"
+    const val WEIGHT_KEY = "weight_key"
+    const val HEIGHT_KEY = "height_key"
+    const val AGE_KEY = "age_key"
+
     // variable for shared preferences.
     //var sharedpreferences: SharedPreferences? = null
     var email: String?=null
     var password:String?=null
     var beforeImage:String?=null
     var afterImage:String?=null
+    var goal:Float=0.0f
+    var weight:Float=0.0f
+    var height:Float=0.0f
+    var age:Int = 0
 }
--- app/src/main/java/com/fournineseven/dietstock/MainActivity.kt
+++ app/src/main/java/com/fournineseven/dietstock/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.Gravity
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
@@ -38,18 +39,22 @@ private const val REQ_STORAGE_BEFORE = 102
 private const val REQ_STORAGE_AFTER = 103
 private const val PERMISSION_REQUEST_CODE = 2
 
-class MainActivity : BaseActivity(), View.OnClickListener {
+class MainActivity : BaseActivity(), View.OnClickListener, UserSettingDialogInterface{
     private lateinit var viewPager: ViewPager2
     private lateinit var navView: BottomNavigationView
-
+    private lateinit var drawnav: Menu
     private lateinit var beforeImage: ImageView
     private lateinit var afterImage: ImageView
     private lateinit var myName: TextView
     private lateinit var myEmail: TextView
-
-    /*private val binding by lazy{
-        ActivityMainBinding.inflate(layoutInflater)
-    }*/
+    private lateinit var myAgeTextView: TextView
+    private lateinit var myHeightTextView: TextView
+    private lateinit var myWeightTextView: TextView
+    private lateinit var myGoalTextView: TextView
+    private val mySettingDialog by lazy{
+        var sharedpreferences = getSharedPreferences(LoginState.SHARED_PREFS, Context.MODE_PRIVATE);
+        UserSettingDialog(this,this,sharedpreferences)
+    }
 
     override fun onClick(v: View?) {
         when (v?.id) {
@@ -62,6 +67,10 @@ class MainActivity : BaseActivity(), View.OnClickListener {
                 val intent = Intent(Intent.ACTION_PICK)
                 intent.type = MediaStore.Images.Media.CONTENT_TYPE
                 startActivityForResult(intent, REQ_STORAGE_AFTER)
+            }
+            R.id.button4->{
+                Log.d(TAG,"THis is button 4")
+                mySettingDialog.show()
             }
         }
     }
@@ -84,53 +93,15 @@ class MainActivity : BaseActivity(), View.OnClickListener {
             .build()
 
         val contentMainViewPager = findViewById<ViewPager2>(R.id.view_pager)
-        val contentMainNavView = findViewById<NavigationView>(R.id.nav_view)
         val bottomNavView = findViewById<BottomNavigationView>(R.id.nav_bottom_view)
-        //val navController = findNavController(R.id.nav_host_fragment)
 
-        beforeImage = contentMainNavView.getHeaderView(0).findViewById(R.id.before_image)
-        afterImage = contentMainNavView.getHeaderView(0).findViewById(R.id.after_image)
+        setUser()
 
-
-        //로그인 상태 확인
-        var sharedpreferences = getSharedPreferences(LoginState.SHARED_PREFS, Context.MODE_PRIVATE);
-        var email: String? = sharedpreferences.getString(LoginState.EMAIL_KEY, null)
-        var password: String? = sharedpreferences.getString(LoginState.PASSWORD_KEY, null)
-        var beforeImageUri: String?= sharedpreferences.getString(LoginState.BEFORE_IMAGE_KEY,null)
-        var afterImageUri:String ?= sharedpreferences.getString(LoginState.AFTER_IMAGE_KEY, null)
-        if (email == null || password == null) {
-            var intent = Intent(this, SignActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-
-        if (beforeImageUri != null) {
-            beforeImage.setImageURI(beforeImageUri!!.toUri())
-        }
-
-        if(afterImageUri != null){
-            afterImage.setImageURI(afterImageUri!!.toUri())
-        }
-
-        if(email == null){
-            myName = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_name)
-            myEmail = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_email)
-            myName.text = "안녕하세요"
-            myEmail.text = "www.naver.com"
-        }
-
-        //viewPager = binding.viewPager
-        //viewPager = contentMainBinding.viewPager
         viewPager = contentMainViewPager
         viewPager.adapter = PagerAdapter(supportFragmentManager, lifecycle)
         viewPager.registerOnPageChangeCallback(PageChangeCallback())
         navView = bottomNavView
         navView.setOnNavigationItemSelectedListener { navigationSelected(it) }
-        /*appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)*/
-        //navView.setupWithNavController(navController)
 
 
         requirePermissions(
@@ -151,7 +122,6 @@ class MainActivity : BaseActivity(), View.OnClickListener {
         finish()
     }
 
-
     override fun onBackPressed() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 
@@ -169,6 +139,74 @@ class MainActivity : BaseActivity(), View.OnClickListener {
             }
         } else {
             super.onBackPressed()
+        }
+    }
+
+    private fun setUser(){
+        val contentMainNavView = findViewById<NavigationView>(R.id.nav_view)
+
+        beforeImage = contentMainNavView.getHeaderView(0).findViewById(R.id.before_image)
+        afterImage = contentMainNavView.getHeaderView(0).findViewById(R.id.after_image)
+        myGoalTextView = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_header_goal)
+        myWeightTextView = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_header_weight)
+        myHeightTextView = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_header_height)
+        myAgeTextView = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_header_age)
+
+
+        //로그인 상태 확인
+        var sharedpreferences = getSharedPreferences(LoginState.SHARED_PREFS, Context.MODE_PRIVATE);
+        var email: String? = sharedpreferences.getString(LoginState.EMAIL_KEY, null)
+        var password: String? = sharedpreferences.getString(LoginState.PASSWORD_KEY, null)
+        var beforeImageUri: String?= sharedpreferences.getString(LoginState.BEFORE_IMAGE_KEY,null)
+        var afterImageUri:String ?= sharedpreferences.getString(LoginState.AFTER_IMAGE_KEY, null)
+        var myGoal:Float= sharedpreferences.getFloat(LoginState.GOAL_KEY,0.0f)
+        var myWeight:Float = sharedpreferences.getFloat(LoginState.WEIGHT_KEY,0.0f)
+        var myHeight:Float = sharedpreferences.getFloat(LoginState.HEIGHT_KEY,0.0f)
+        var myAge:Int = sharedpreferences.getInt(LoginState.AGE_KEY,0)
+
+
+        /*if (email == null || password == null) {
+            var intent = Intent(this, SignActivity::class.java)
+            startActivity(intent)
+            finish()
+        }*/
+
+        //초기화
+        if (beforeImageUri != null) {
+            beforeImage.setImageURI(beforeImageUri!!.toUri())
+        }
+
+        if(afterImageUri != null){
+            afterImage.setImageURI(afterImageUri!!.toUri())
+        }
+        /*myGoalTextView.text = myGoal.toString()
+        myHeightTextView.text = myHeight.toString()
+        myWeightTextView.text = myWeight.toString()
+        myAgeTextView.text = myAge.toString()*/
+        myAgeTextView.text = "나이 : " + myAge.toString() + "살"
+        myGoalTextView.text = "목표 : " + myGoal.toString() + "KG"
+        myWeightTextView.text = "몸무게 : " + myWeight.toString() + "KG"
+        myHeightTextView.text = "키 : " + myHeight.toString() +"CM"
+
+
+        //입력하지 않았을 경우.
+        if(email == null){
+            myName = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_name)
+            myEmail = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_email)
+            myName.text = "나의 이름 입력값 없음"
+            myEmail.text = "나의 이메일 입력값 없음"
+        }
+        if(myGoal < 0.2f){
+            myGoalTextView.text = "목표 입력값 없음"
+        }
+        if(myAge <1){
+            myAgeTextView.text = "나이 입력값 없음"
+        }
+        if(myHeight < 0.2f){
+            myHeightTextView.text = "키 입력값 없음"
+        }
+        if(myWeight < 0.2f){
+            myWeightTextView.text = "몸무게 입력값 없음"
         }
     }
 
@@ -243,7 +281,6 @@ class MainActivity : BaseActivity(), View.OnClickListener {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 
-
         if (item.itemId == android.R.id.home) {
             if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
                 drawerLayout.closeDrawer(Gravity.LEFT)
@@ -284,5 +321,30 @@ class MainActivity : BaseActivity(), View.OnClickListener {
                 }
             }
         }
+    }
+
+    override fun onCancelButtonClicked() {
+        Log.d(TAG,"취소버튼 클릭")
+    }
+
+    override fun onOkButtonClicked() {
+        Log.d(TAG,"확인버튼 클릭")
+
+        val contentMainNavView = findViewById<NavigationView>(R.id.nav_view)
+        myGoalTextView = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_header_goal)
+        myWeightTextView = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_header_weight)
+        myHeightTextView = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_header_height)
+        myAgeTextView = contentMainNavView.getHeaderView(0).findViewById(R.id.nav_header_age)
+
+        var sharedpreferences = getSharedPreferences(LoginState.SHARED_PREFS, Context.MODE_PRIVATE);
+        var myGoal:Float= sharedpreferences.getFloat(LoginState.GOAL_KEY,0.0f)
+        var myWeight:Float = sharedpreferences.getFloat(LoginState.WEIGHT_KEY,0.0f)
+        var myHeight:Float = sharedpreferences.getFloat(LoginState.HEIGHT_KEY,0.0f)
+        var myAge:Int = sharedpreferences.getInt(LoginState.AGE_KEY,0)
+
+        myAgeTextView.text = "나이 : " + myAge.toString() + "살"
+        myGoalTextView.text = "목표 : " + myGoal.toString() + "KG"
+        myWeightTextView.text = "몸무게 : " + myWeight.toString() + "KG"
+        myHeightTextView.text = "키 : " + myHeight.toString() +"CM"
     }
 }
